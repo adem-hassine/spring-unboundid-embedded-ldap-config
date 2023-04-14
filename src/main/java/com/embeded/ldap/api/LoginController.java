572ece2659cb55126d5af662812b6c211bbcb07b
Adem HASSINE <adem.hassine@proxym-it.com>
2023-04-14 03:19:41 +0100

    login controller with authentication manager manual call

---
+++ src/main/java/com/embeded/ldap/api/LoginController.java
@@ -0,0 +1,32 @@
+package com.embeded.ldap.api;
+
+import com.embeded.ldap.JwtUtils;
+import com.embeded.ldap.constants.UtilityConstants;
+import com.embeded.ldap.dto.AuthenticationRequest;
+import com.embeded.ldap.dto.AuthenticationResponse;
+import lombok.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LoginController {
+
+
+
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+
+    @PostMapping(UtilityConstants.LOGIN_ENDPOINT)
+    public AuthenticationResponse greeting(@RequestBody AuthenticationRequest authenticationRequest) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+                authenticationRequest.getPassword());
+         authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        return AuthenticationResponse.builder()
+                .token(this.jwtUtils.generateToken(authenticationRequest.getUsername())).username(authenticationRequest.getUsername()).build();
+    }
+}
