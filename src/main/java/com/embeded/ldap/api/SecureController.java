4063d52c7e0f713c62472b49fa2e78df8edfbdfc
Adem HASSINE <adem.hassine@proxym-it.com>
2023-04-14 03:17:25 +0100

    secure controller for testing POST token responded behaviour

---
+++ src/main/java/com/embeded/ldap/api/SecureController.java
@@ -0,0 +1,22 @@
+package com.embeded.ldap.api;
+
+import com.embeded.ldap.entry.UserEntry;
+import com.embeded.ldap.entry.UserEntryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/secure")
+@RequiredArgsConstructor
+public class SecureController {
+    private final UserEntryRepository userEntryRepository;
+    @GetMapping("/users")
+    public ResponseEntity<List<UserEntry>> greeting(){
+        return ResponseEntity.ok(userEntryRepository.findAll());
+    }
+}
